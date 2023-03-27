6591c1ae6eb619fc8cdee4c47ad5da4e3b45bc93
morjay007 <mojekwudanielprince@icloud.com>
2023-03-27 08:05:37 +0100

    update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-
- Sample code for the flask web app 
- 
 from flask import Flask 
+
 app = Flask(__name__) 
  
-@app.route("/") def hello(): 
+@app.route("/") 
+def hello(): 
     return "I am almost a Devops Engineer!" 
  
-if __name__ == "__main__":     app.run(host='0.0.0.0')
+if __name__ == "__main__":     
+    app.run()
